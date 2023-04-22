f15107460fbec92896ebd9ea74059ff5d38320a3
0x11901 <wjk930726@live.com>
2023-04-22 13:40:42 +0800

    Last time I said it works? I was kidding.  Try this.

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,8 @@ import Data.Aeson
 import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
+-- 貌似https用到的，我不需要用
+-- import Network.Wai.Handler.WarpTLS
 import Servant
 
 data User = User
@@ -20,8 +22,10 @@ data User = User
 
 $(deriveJSON defaultOptions ''User)
 
+-- 这里意思就是的访问 127.0.0.1:8080/users
 type API = "users" :> Get '[JSON] [User]
 
+-- 此处定义端口为8080
 startApp :: IO ()
 startApp = run 8080 app
 
@@ -38,3 +42,14 @@ users :: [User]
 users = [ User 1 "Isaac" "Newton"
         , User 2 "Albert" "Einstein"
         ]
+
+-- type API = Get '[JSON] Int
+
+-- api :: Proxy API
+-- api = Proxy
+
+-- server :: Server API
+-- server = return 10
+
+-- app :: Application
+-- app = serve api server
